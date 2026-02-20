5c432de7eaaa4378092f51ce10c699212ef38c6c
AirenMarie <astringer92@gmail.com>
2026-02-19 23:19:27 -0500

    change source file for express.static

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static("./public"));
+app.use(express.static("./front"));
 app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
