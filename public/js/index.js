5408f475ea33652ff8ce4b84bd2c2484b28f82c8
AirenMarie <astringer92@gmail.com>
2026-03-22 17:38:50 -0400

    change name of imageAndQuote; add fetchQuotes

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,39 @@
-async function imageAndQuote() {
+async function fetchImages() {
   try {
-    const endpoint = "http://localhost:3000/api/v1/imageAndQuote";
+    const endpoint = "http://localhost:3000/api/v1/fetchImages";
 
     const response = await fetch(endpoint);
     const result = await response.json();
+    const imgUrl = result.data;
 
     console.log(result);
+
+    const quoteCard = document.querySelector("#quote-section");
+    quoteCard.style.backgroundImage = `url("${imgUrl}")`;
   } catch (error) {
     console.error(error);
   }
 }
 
-imageAndQuote();
+async function fetchQuotes() {
+  try {
+    const endpoint = "http://localhost:3000/api/v1/fetchQuotes";
+
+    const response = await fetch(endpoint);
+    const result = await response.json();
+    const data = result[0];
+
+    console.log(data);
+
+    const quoteText = document.querySelector("#quote");
+
+    if (quoteText && data) {
+      quoteText.innerHTML = `<p><i>"${data.quote}"</i>--${data.author}</p>`;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchImages();
+fetchQuotes();
