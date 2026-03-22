ffb0bc2574b0972f7b8975b6970ed8d4866f48ea
AirenMarie <astringer92@gmail.com>
2026-03-22 17:40:53 -0400

    change name of imageAndQuote; add fetchImages and new app.get

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-async function imageAndQuote() {
+async function fetchImages() {
   try {
     const image = `https://api.unsplash.com/topics/spring/photos?client_id=${process.env.UNSPLASH_KEY}`;
 
@@ -31,13 +31,49 @@ async function imageAndQuote() {
   }
 }
 
-app.get("/api/v1/imageAndQuote", async (request, response) => {
+async function fetchQuotes() {
   try {
-    const images = await imageAndQuote();
+    const quote =
+      "https://api.api-ninjas.com/v2/randomquotes?categories=wisdom,courage,success";
+    const api_key = process.env.QUOTES_KEY;
+
+    const response = await fetch(quote, {
+      headers: { "X-Api-Key": api_key },
+    });
+    const data = await response.json();
+
+    if (data.length > 0) {
+      console.log("Success! Your quote:", data[0]);
+    } else {
+      console.log("No quotes found in this category.");
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.get("/api/v1/fetchImages", async (request, response) => {
+  try {
+    const images = await fetchImages();
 
     response.status(200).json({
       status: 200,
       data: images,
+    });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/v1/fetchQuotes", async (request, response) => {
+  try {
+    const quotes = await fetchQuotes();
+
+    response.status(200).json({
+      status: 200,
+      data: quotes,
     });
   } catch (error) {
     response.status(500).json({ error: error.message });
